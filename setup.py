fb643936f3f3d470acf156f315b7376d2d22d046
cy-xu <hello.xu@gmail.com>
2021-12-06 12:30:54 -0800

    trained models added to git repo

--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cy-xu/cosmic-conn",
-    version="0.2.5",
+    version="0.2.6",
     zip_safe=False,
 )
